52765a12d9eddccfa9dcd24833be51d885bb7d4c
Francis Bourre <francis_bourre@me.com>
2017-02-23 12:46:35 +0100

    Stringifier moved to util package

--- src/hex/compiletime/basic/CompileTimeCoreFactory.hx
+++ src/hex/compiletime/basic/CompileTimeCoreFactory.hx
@@ -9,7 +9,7 @@ import hex.error.IllegalArgumentException;
 import hex.error.NoSuchElementException;
 import hex.event.ClosureDispatcher;
 import hex.event.MessageType;
-import hex.log.Stringifier;
+import hex.util.Stringifier;
 
 /**
  * ...
