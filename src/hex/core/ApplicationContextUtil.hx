055bae7da056e60709d54960033ae3f43b900511
Laurent Deketelaere <Laurent.Deketelaere@doclerholding.com>
2017-06-26 13:22:00 +0200

    add ApplicationContextUtil valide name regex and tests

--- src/hex/core/ApplicationContextUtil.hx
+++ src/hex/core/ApplicationContextUtil.hx
@@ -1,19 +1,22 @@
 package hex.core;
 
-#if macro
-import haxe.macro.*;
-
 /**
  * ...
  * @author Francis Bourre
  */
 class ApplicationContextUtil 
 {
+	static var variableNameEreg = ~/[a-zA-Z_\x7f-\xff][a-zA-Z0-9_\x7f-\xff]*/;
+
 	/** @private */ function new() throw new hex.error.PrivateConstructorException();
 	
-	static public function validateName( name : String )
+	static public function isValidName( name : String ) : Bool
 	{
+		if( variableNameEreg.match( name ) )
+		{
+			return variableNameEreg.matched(0) == name;
+		}
+		return false;
 		//Context.error( 'Invalid application context name.\n Name should be alphanumeric (underscore is allowed).\n First chararcter should not be a number.', Context.currentPos() );
 	}
 }
-#end
