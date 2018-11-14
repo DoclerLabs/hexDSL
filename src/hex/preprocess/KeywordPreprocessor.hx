3f0ba450bfb27a5bb982489ab10c77113770c526
Francis Bourre <francis_bourre@me.com>
2018-11-14 11:47:16 +0100

    Better regex to prevent to catch #if with two more keywords (and, or)

--- src/hex/preprocess/KeywordPreprocessor.hx
+++ src/hex/preprocess/KeywordPreprocessor.hx
@@ -13,10 +13,22 @@ class KeywordPreprocessor
     #if macro
     static public function parse( data : String ) : String
 	{
-		data = ~/(\bif\b)(\s*)(\()+/g.map
+		data = ~/(?<![$&+,:;=?@#|'<>.^*()%!-])(\bif\b)(\s*)(\()+/g.map
+		//data = ~/(\bif\b)(\s*)(\()+/g.map
 		(
-			data,
-			function ( ereg ) return '_if('
+			data, function ( ereg ) return '_if('
+		);
+
+		data = ~/(?<![$&+,:;=?@#|'<>.^*()%!-])(\bif\b)(\s*)(\()+/g.map
+		//data = ~/(\band\b)(\s*)(\()+/g.map
+		(
+			data, function ( ereg ) return '_and('
+		);
+
+		data = ~/(?<![$&+,:;=?@#|'<>.^*()%!-])(\bif\b)(\s*)(\()+/g.map
+		//data = ~/(\bor\b)(\s*)(\()+/g.map
+		(
+			data, function ( ereg ) return '_or('
 		);
 
 		return data;
