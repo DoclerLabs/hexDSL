8434d21876bb4a21ee2ecb7dfed74e686cec7afa
Francis Bourre <francis_bourre@me.com>
2018-11-16 10:59:02 +0100

    Let's clean it

--- src/hex/compiletime/factory/BoolOpFactory.hx
+++ src/hex/compiletime/factory/BoolOpFactory.hx
@@ -19,37 +19,18 @@ class BoolOpFactory
 	{
 		var constructorVO 	= factoryVO.constructorVO;
 		var idVar 			= constructorVO.ID;
-		var args 			= constructorVO.arguments;
 		var e 				= constructorVO.arguments.shift();
 
-		var builtArgs = ( constructorVO.arguments.length > 0 ) ? ArgumentFactory.build( factoryVO, constructorVO.arguments ) : [];
-		builtArgs.shift();
-		builtArgs.shift();
-		setVO( builtArgs, constructorVO, factoryVO.contextFactory.getTypeLocator() );
+		ArgumentFactory.build( factoryVO, constructorVO.arguments );
 
 		//Building result
+		constructorVO.cType = macro :Bool;
+		constructorVO.type = constructorVO.fqcn = 'Bool';
+
 		var t = constructorVO.cType;
 		return constructorVO.shouldAssign && !constructorVO.lazy ?
 			macro @:pos( constructorVO.filePosition ) var $idVar : $t = $e:
 			macro @:pos( constructorVO.filePosition ) $e;	
-	}
-
-	static function setVO( builtArgs : Array<Expr>, constructorVO : hex.vo.ConstructorVO, typelocator : hex.collection.ILocator<String, String> )
-	{
-		var ids = builtArgs.map(
-			function ( el )
-				return switch( el.expr )
-				{
-					case EConst(CIdent(ident)): TypeTools.toComplexType(haxe.macro.Context.getType(typelocator.locate(ident)));
-					case _: TypeTools.toComplexType( Context.typeof(el));
-				}
-		).map( function( el ) return macro (null:$el) );
-
-		if( ids.length >  0 ) 
-			constructorVO.cType = TypeTools.toComplexType(haxe.macro.Context.typeof(macro $a{ids}[0])); 
-				else _blankType( constructorVO );
-
-		constructorVO.type = constructorVO.fqcn = hex.util.MacroUtil.getFQCNFromComplexType( constructorVO.cType );
 	}
 }
 #end
